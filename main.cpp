9078d857e0bafb8da85306ae8513cb74c552318d
Kevin Rives <kevinrives@MacBook-Pro-de-Kevin.local>
2022-04-07 20:01:42 +0200

    documentation added

--- main.cpp
+++ main.cpp
@@ -2,20 +2,27 @@
 
 #include "server.h"
 
+/** \brief              Main entry point of a server
+ *  \param[in]          argc Args count
+ *  \param[out]         argv Arguments array
+ *  \return             integer
+**/
 int main(int argc, char* argv[])
 {
     try
     {
+        // Test for the presence of the port of the server
         if (argc != 2)
         {
             std::cerr << "Usage: async_tcp_echo_server <port>\n";
             return 1;
         }
 
+        // Creation of a server instance
         boost::asio::io_service ioContext;
-
         Server s(ioContext, std::atoi(argv[1]));
 
+        // Run the server
         ioContext.run();
     }
     catch (std::exception& e)
--- server.h
+++ server.h
@@ -6,19 +6,47 @@
 
 using boost::asio::ip::tcp;
 
+/** \name               Server
+ *  \brief              Class that permits to handle several clients
+**/
 class Server
 {
 public:
 
+    /*******************************************************
+     * \section             Constructor, destructor
+     *
+    *******************************************************/
+
+    /** \brief              Assign constructor
+     *  \param[in]          ioContext_ Mandatory for creating a server
+     *  \param[in]          port_ Server port on which clients will connect
+    **/
     Server(boost::asio::io_service& ioContext_, uint16_t port_);
 
+    /** \brief              Default destructor
+    **/
     ~Server() = default;
 
 private:
 
+    /*******************************************************
+     * \section             Private functions and methods
+     *
+    *******************************************************/
+    
+    /** \brief              Accept for new clients (will create a Session for each client)
+    **/
     void accept();
 
+    /*******************************************************
+     * \section             Private members
+     *
+    *******************************************************/
+    
+    /// \brief              Acceptor for new clients
     tcp::acceptor m_acceptor;
 
+    /// \brief              Socket on which clients will connect (Data will be sent and receive thanks to this)
     tcp::socket m_socket;
 };
